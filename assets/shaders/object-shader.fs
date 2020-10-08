a94b2da66f80d3399c0660a3e8a89e118f48de4a
Konstantin Lyubort <lyubortk@gmail.com>
2020-10-08 18:25:01 +0300

    implement reflection and refraction

--- assets/shaders/object-shader.fs
+++ assets/shaders/object-shader.fs
@@ -16,5 +16,19 @@ void main()
     vec3 normalized_normal = normalize(normal);
     vec3 reflection_vec = reflect(normalized_vision_vec, normalized_normal);
     vec3 refraction_vec = refract(normalized_vision_vec, normalized_normal, 1.0 / u_refractive_index);
-    o_frag_color = vec4(texture(u_environment, refraction_vec).rgb, 1.0);
+
+    vec3 reflection_color = texture(u_environment, reflection_vec).rgb;
+    vec3 refraction_color = texture(u_environment, refraction_vec).rgb;
+
+    float theta_t = dot(refraction_vec, -normalized_normal);
+    float theta_i = dot(-normalized_vision_vec, normalized_normal);
+
+    float z2 = 1.0 / u_refractive_index;
+    float rs_root = (z2 * theta_i - theta_t) / (z2 * theta_i + theta_t);
+    float rp_root = (z2 * theta_t - theta_i) / (z2 * theta_t + theta_i);
+
+    float reflection_coeff = (rs_root * rs_root + rp_root * rp_root) / 2;
+    vec3 final_color = reflection_coeff * reflection_color + (1 - reflection_coeff) * refraction_color;
+
+    o_frag_color = vec4(final_color, 1.0);
 }
